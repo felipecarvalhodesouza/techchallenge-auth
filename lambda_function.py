b1edc4c1bc01d4cb9ea2d3c277df2de53257864b
Felipe Carvalho de Souza <36648569+felipecarvalhodesouza@users.noreply.github.com>
2024-07-12 21:50:30 -0300

    Alterando lambda para permitir a criação de usuários

--- lambda_function.py
+++ lambda_function.py
@@ -1,34 +1,68 @@
 import boto3
 import json
+from botocore.exceptions import ClientError
 
-cognito = boto3.client('cognito-idp')
+client = boto3.client('cognito-idp', region_name='us-east-1')
+
 def lambda_handler(event, context):
-    try:
-        body = json.loads(event['body'])
-        username = body['username']
-        password = body['password']
-        user_pool_id = 'us-east-1_sM99wlo4I'
-        client_id = 'qhahmrq7493f7ncp6jh4httlh'
-        response = cognito.initiate_auth(
-            AuthFlow='USER_PASSWORD_AUTH',
-            AuthParameters={
-                'USERNAME': username,
-                'PASSWORD': password
-            },
-            ClientId=client_id
-        )
-        return {
-            'statusCode': 200,
-            'headers': {
-                'Access-Control-Allow-Origin': '*'
-            },
-            'body': json.dumps(response)
-        }
-    except Exception as e:
-        return {
-            'statusCode': 200,
-            'headers': {
-                'Access-Control-Allow-Origin': '*'
-            },
-            'body': json.dumps(e)
-        }
+    
+    request_body = event
+
+    if request_body['operation'] == 'register':
+
+        try:
+            create_user_params = {
+                'UserPoolId': 'us-east-1_pGFsCFVuS',
+                'Username': request_body['email'],
+                'TemporaryPassword': request_body['temporaryPassword'],
+                'UserAttributes': [
+                    {'Name': 'email', 'Value': request_body['email']},
+                    {'Name': 'email_verified', 'Value': 'true'}
+                ]
+            }
+
+            response = client.admin_create_user(**create_user_params)
+
+            return {
+                'statusCode': 200,
+                'body': json.dumps({'message': 'User created successfully', 'user': str(response['User'])})
+            }
+        except ClientError as e:
+            print(f"User creation error: {e.response['Error']['Message']}")
+            return {
+                'statusCode': 500,
+                'body': json.dumps({'message': 'Failed to create user'})
+            }
+
+    else: 
+        try:
+            username = request_body['email']
+            password = request_body['password']
+			
+            user_pool_id = 'us-east-1_pGFsCFVuS'
+            client_id = '3mmedbc37gunvdc708k2s6nsiu'
+
+            response = client.initiate_auth(
+				AuthFlow='USER_PASSWORD_AUTH',
+				AuthParameters={
+					'USERNAME': username,
+					'PASSWORD': password
+				},
+				ClientId=client_id
+			)
+
+            return {
+				'statusCode': 200,
+				'headers': {
+					'Access-Control-Allow-Origin': '*'
+				},
+				'body': json.dumps(str(response))
+			}
+        except Exception as e:
+            return {
+				'statusCode': 403,
+				'headers': {
+					'Access-Control-Allow-Origin': '*'
+				},
+				'body': json.dumps(str(e))
+			}
